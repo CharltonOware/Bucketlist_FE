cfa8282aab0997907e0e20cd43c034ecf0c5e600
Charlton Omondi <omondioware@gmail.com>
2021-08-29 17:21:16 +0300

    Refactored submit event listeners from promises to async await

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Handler for clicking on the create account link
     document.querySelector("#linkCreateAccount").addEventListener("click", e=> {
-      e.preventDefault();
-      loginForm.classList.add("form__hidden");
-      createAccountForm.classList.remove("form__hidden");  
+        e.preventDefault();
+        loginForm.classList.add("form__hidden");
+        createAccountForm.classList.remove("form__hidden");  
     });
     //Handler for clicking on the login link
     document.querySelector("#linkLogin").addEventListener("click", e=> {
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     //Handler for submitting during the account creation process
-    createAccountForm.addEventListener("submit", e=> {
+    createAccountForm.addEventListener("submit", async e => {
         e.preventDefault();
         //Register new user using fetch()
         const email = document.querySelector("#signupEmail").value;
@@ -47,40 +47,35 @@ document.addEventListener("DOMContentLoaded", () => {
         } catch (error) {
             console.log("Error", error);
         }
-    })
+    });
     //Handler for submitting during the login process
-    loginForm.addEventListener("submit", e=> {
+    loginForm.addEventListener("submit", async e=> {
         e.preventDefault();
         //fetch login
         const email = document.querySelector("#login-email").value;
         const password = document.querySelector("#login-pass").value;
         console.log(email, password);
-        return fetch(loginurl, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "email": email,
-                "password": password,
-            })
-        })
-        .then( res => {
-            console.log('FULL RESPONSE ',res);
-            return res.json()
-
-        })
-        .then(data => {
-            console.log('DATA',data);
-            localStorage.setItem('auth_token',JSON.stringify(data))
-            //window.location.push("/home/bucketlist.html");
-        })
-        .catch(error => {
+        try {
+            const res = await fetch(loginurl, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "email": email,
+                    "password": password,
+                })
+            });
+            console.log('FULL RESPONSE ', res);
+            const data_1 = await res.json();
+            console.log('DATA', data_1);
+            localStorage.setItem('auth_token', JSON.stringify(data_1));
+        } catch (error) {
             //enter logic when there is an error
-            console.log(error)
-        });
-    })
+            console.log(error);
+        }
+    });
 
     // document.querySelectorAll(".form__input").forEach(inputElement => {
     //     inputElement.addEventListener("blur", e => {
